aad9b9b17e81f893ae71758641b8a7f1529aae3a
tykm <tylerkim01@gmail.com>
2021-12-04 23:41:51 -0500

    Made the first GET api endpoint. Hello World

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const PORT = process.env.PORT || 3001;      // Runs on port from .env var 'PORT'
 const express = require("express");
 const app = express();
 
+// Creating endpoint for {url}/api
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
