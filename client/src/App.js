7e4a27b6fc97b09bff43d16ee8004d660ad8e3f2
tykm <tylerkim01@gmail.com>
2021-12-06 14:26:36 -0500

    Added a textarea, input box, and submit button

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>{ Textbox() }</p>
+        { Textbox() }
       </header>
     </div>
   );
--- client/src/components/Textbox.js
+++ client/src/components/Textbox.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function Textbox() {
+    const [lines, setLines] = useState('');
+    const [text, setText] = useState('');
 
+    function updateText(txt) {
+        setText(txt);
+    }
+
+    function submitLines() {
+        setLines({ text });
+    }
 
     return(
-        <input type='text' name='typing'/>
+        <div>
+            <textarea name='displayLines' value={ lines } /><br/>
+            <input type='text' name='line' onChange={ e => updateText(e.target.value) }/><br/>
+            <input type='button' name='submitLine' onClick={ submitLines }/>
+        </div>
     );
 }
 
